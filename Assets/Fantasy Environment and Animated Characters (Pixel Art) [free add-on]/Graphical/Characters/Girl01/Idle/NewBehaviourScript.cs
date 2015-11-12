130375c4f637a5e5ea835439d502f6713d24cb68
Matthew Huber <Matthew Huber>
2015-11-11 22:11:51 -0800

    initial commit

---
+++ Assets/Fantasy Environment and Animated Characters (Pixel Art) [free add-on]/Graphical/Characters/Girl01/Idle/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public float speed;
+
+	void FixedUpdate(){
+		var mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
